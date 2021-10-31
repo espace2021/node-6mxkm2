c7f3ff0657523ccf8bb711e9623a967fcabfc34a
espace2021 <92257192+espace2021@users.noreply.github.com>
2021-10-31 15:45:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,25 +12,42 @@ import articleRouter from "./routes/article.route.js"
 //BodyParser Middleware
 app.use(express.json()); 
 app.use(cors());
-// Connexion à la base données
-mongoose.connect(process.env.DATABASECLOUD,{
- useNewUrlParser: true,
- useUnifiedTopology: true
- })
- .then(() => {console.log("Connexion à la base de données réussie");
-}).catch(err => {
- console.log('Impossible de se connecter à la base de données', err);
- process.exit();
+
+//PROMPT
+import readline from 'readline';
+const rl = readline.createInterface({             
+  input: process.stdin,
+  output: process.stdout
 });
 
-app.use('/api/categories', categorieRouter);
+rl.question('Quelle est la base de données ? ', (answer) => {  
+  console.log(`Base de données : ${answer} !`);
+    // Connexion à la base données
+
+mongoose.connect(answer,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then(() => {console.log("Connexion à la base de données réussie");
+   }).catch(err => {
+    console.log('Impossible de se connecter à la base de données', err);
+    process.exit();
+   
+});
+
+
+app.use('/api/categories/', categorieRouter);
 app.use('/api/scategories', scategorieRouter);
 app.use('/api/articles', articleRouter);
+
+
+rl.close();                                     
+});
 
 app.get("/",(req,res)=>{
 res.send("formation");
 });
 
+
 app.listen(process.env.PORT, () => {
  console.log(`Server is listening on port ${process.env.PORT}`); });
-
